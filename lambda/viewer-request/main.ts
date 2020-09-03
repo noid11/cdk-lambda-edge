64c597a4e69fd0a42ed53a2a1517281a621ae9a3
noid11 <12472231+noid11@users.noreply.github.com>
2020-09-04 03:18:40 +0900

    implement: minimum viewer request lambda edge function

---
+++ lambda/viewer-request/main.ts
@@ -0,0 +1,8 @@
+export const handler = (event: any = {}, context: any = {}, callback: any = {}) => {
+
+    console.log(event);
+
+    const request = event.Records[0].cf.request;
+
+    callback(null, request);
+};
--- lib/cdk-lambda-edge-stack.ts
+++ lib/cdk-lambda-edge-stack.ts
@@ -1,14 +1,58 @@
 import * as cdk from '@aws-cdk/core';
 import * as cloudfront from '@aws-cdk/aws-cloudfront';
 import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as iam from '@aws-cdk/aws-iam';
 
 export class CdkLambdaEdgeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const lambdaEdgeIamRole = new iam.Role(this, 'lambdaEdgeIamRole', {
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal('edgelambda.amazonaws.com'),
+        new iam.ServicePrincipal('lambda.amazonaws.com')
+      )
+    });
+
+    const allowAssumeRoleStatement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["sts:AssumeRole"],
+      resources: ["*"]
+    });
+
+    const allowLogsStatement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      resources: ["*"]
+    });
+
+    const assumePolicy = new iam.Policy(this, 'StsAssumeForLambdaEdge');
+    assumePolicy.addStatements(allowAssumeRoleStatement);
+    assumePolicy.addStatements(allowLogsStatement);
+
+    lambdaEdgeIamRole.attachInlinePolicy(assumePolicy);
+
+    const viewerRequestFunction = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda/viewer-request'),
+      handler: 'main.handler',
+      role: lambdaEdgeIamRole
+    });
+
     const dist = new cloudfront.Distribution(this, 'myDist', {
       defaultBehavior: {
-        origin: new origins.HttpOrigin('www.example.com')
+        origin: new origins.HttpOrigin('www.example.com'),
+        edgeLambdas: [
+          {
+            functionVersion: viewerRequestFunction.currentVersion,
+            eventType: cloudfront.LambdaEdgeEventType.VIEWER_REQUEST,
+          }
+        ]
       }
     });
   }
