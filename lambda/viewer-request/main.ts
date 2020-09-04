2ce16e1653eb50cd42949b9831c54eeec87e6aa7
noid11 <12472231+noid11@users.noreply.github.com>
2020-09-04 16:44:12 +0900

    dump request event at all lambda edge functions

--- lambda/viewer-request/main.ts
+++ lambda/viewer-request/main.ts
@@ -1,5 +1,5 @@
 export const handler = (event: any = {}, context: any = {}, callback: any = {}) => {
-    console.log(event);
+    console.log(JSON.stringify(event, null, 4));
     const request = event.Records[0].cf.request;
     callback(null, request);
 };
--- lambda/viewer-response/main.ts
+++ lambda/viewer-response/main.ts
@@ -1,5 +1,5 @@
 export const handler = (event: any = {}, context: any = {}, callback: any = {}) => {
-    console.log(event);
+    console.log(JSON.stringify(event, null, 4));
     const response = event.Records[0].cf.response;
     callback(null, response);
 };
